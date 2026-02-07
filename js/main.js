9739351fba3169f9dac1bb2b70b6ad6aaf5f39de
Special-Rapid <kenta.sakaki.2010@gmail.com>
2026-02-07 18:02:13 +0900

    Update demo video and add anchor smooth-scrolling
    Replace img/WebD_Minecraft_Site.mp4 and add smooth-scrolling for internal anchor links in js/main.js. The new click handler targets links with href starting with '#', calculates the target offset and animates the page scroll over 700ms using the 'swing' easing (returns false to prevent default). An 'adjust' variable is defined (0) for potential offset but not applied.

--- js/main.js
+++ js/main.js
@@ -10,4 +10,13 @@ $(function ($) {
             }
         });
     });
+    $(".container ul li a[href^='#']").click(function () {
+        var adjust = 0;
+        var speed = 700;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? "html" : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
 });
